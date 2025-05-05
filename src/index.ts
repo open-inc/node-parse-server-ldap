f477a3b5e49d1f580a8d221f77f1b0053071adac
lrilling <lukas.rilling@gmail.com>
2025-05-05 17:09:15 +0200

    Setting default tenant using env variable

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,8 @@ let PARSE_LDAP_EXPIRE_LENGTH = process.env.PARSE_LDAP_EXPIRE_LENGTH
 
 const PARSE_LDAP_UNIFY_CREDENTIALS = process.env.PARSE_LDAP_UNIFY_CREDENTIALS === "true";
 
+const PARSE_LDAP_DEFAULT_TENANT_ID = process.env.PARSE_LDAP_DEFAULT_TENANT_ID || undefined;
+
 async function init() {
   if (!PARSE_LDAP_URL) {
     console.log("Parse LDAP Plugin is not active. PARSE_LDAP_URL is required.");
@@ -170,6 +172,12 @@ async function init() {
       user_c.set(PARSE_LDAP_PARSE_LDAP_ATTRIBUTE, true);
       user_c.set(PARSE_LDAP_PARSE_LDAP_DN_ATTRIBUTE, user.dn);
       user_c.set("password", token);
+
+      if (PARSE_LDAP_DEFAULT_TENANT_ID !== undefined) {
+        const tenant = new Parse.Object("OD3_Tenant");
+        tenant.id = PARSE_LDAP_DEFAULT_TENANT_ID;
+        user_c.set("tenant", tenant);
+      }
 
       await user_c.signUp();
 
